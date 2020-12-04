b8e04ed281b9ea47b6ff5085e4684375e4659bcc
Ayan <dot.developer404@gmail.com>
2020-12-04 17:37:57 +0500

    pushing code

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+//phpinfo();
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix' => '2fa'], function () {
+    Route::get('/', 'LoginSecurityController@show2faForm');
+    Route::post('/generateSecret', 'LoginSecurityController@generate2faSecret')->name('generate2faSecret');
+    Route::post('/enable2fa', 'LoginSecurityController@enable2fa')->name('enable2fa');
+    Route::post('/disable2fa', 'LoginSecurityController@disable2fa')->name('disable2fa');
+
+    // 2fa middleware
+    Route::post('/2faVerify', function () {
+        return redirect(URL()->previous());
+    })->name('2faVerify')->middleware('2fa');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
